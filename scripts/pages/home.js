b664f8a4ca9b1287ad642cbcd8ba251fbaa5ad12
Chukwudi Eze <chukwudieze97@gmail.com>
2023-05-01 13:08:45 +0100

    add button navigation

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -21,6 +21,7 @@ export class Home extends Component {
     this.onBoxMouseOver();
     this.onBoxMouseOut();
     this.onSelectBox();
+    this.onNavButtonClick();
   }
 
   onBoxMouseOver() {
@@ -56,62 +57,29 @@ export class Home extends Component {
   onSelectBox() {
     forEach(this.elements.boxes, (box) => {
       box.addEventListener("click", () => {
-        if (
-          box.classList.contains("active") ||
-          !box.classList.contains("in-view")
-        )
-          return;
+        this.handleBoxSelection(box);
+      });
+    });
+  }
 
-        this.resetActiveBoxes();
-        box.classList.add("active");
-
-        if (
-          this.elements.boxesInView[
-            this.elements.boxesInView.length - 1
-          ].getAttribute("data-box") === box.getAttribute("data-box") &&
-          box.getAttribute("data-box") !==
-            this.elements.boxes[this.elements.boxes.length - 1].getAttribute(
-              "data-box"
-            )
-        ) {
-          this.elements.boxesInView[0].classList.remove("in-view");
-          this.elements.boxes[
-            Number(box.getAttribute("data-box")) + 1
-          ].classList.add("in-view");
-          const firstBox = this.elements.boxes[0];
-          this.elements.boxInnerWrapper.removeChild(
-            this.elements.boxInnerWrapper.firstElementChild
+  onNavButtonClick() {
+    forEach(this.elements.navButton, (button) => {
+      button.addEventListener("click", () => {
+        let box = null;
+        let index = Number(this.elements.activeBox.getAttribute("data-box"));
+        if (button.className === "left") {
+          index = index === 0 ? this.elements.boxes.length : index;
+          box = Array.from(this.elements.boxes).find(
+            (elem) => Number(elem.getAttribute("data-box")) === index - 1
           );
-          this.elements.boxInnerWrapper.appendChild(firstBox);
-          this.create();
-          return;
         }
-
-        if (
-          this.elements.boxesInView[0].getAttribute("data-box") ===
-            box.getAttribute("data-box") &&
-          box.getAttribute("data-box") !==
-            this.elements.boxes[0].getAttribute("data-box")
-        ) {
-          this.elements.boxesInView[
-            this.elements.boxesInView.length - 1
-          ].classList.remove("in-view");
-          this.elements.boxes[
-            Number(box.getAttribute("data-box")) - 1
-          ].classList.add("in-view");
-          const lastBox = this.elements.boxes[this.elements.boxes.length - 1];
-          this.elements.boxInnerWrapper.removeChild(
-            this.elements.boxInnerWrapper.lastElementChild
-          );
-          this.elements.boxInnerWrapper.insertBefore(
-            lastBox,
-            this.elements.boxInnerWrapper.firstElementChild
-          );
-          this.create();
-          return;
+        if (button.className === "right") {
+            index = index === this.elements.boxes.length ? -1 : index;
+            box = Array.from(this.elements.boxes).find(
+              (elem) => Number(elem.getAttribute("data-box")) === index + 1
+            );
         }
-
-        this.create(false);
+        this.handleBoxSelection(box);
       });
     });
   }
@@ -131,7 +99,63 @@ export class Home extends Component {
   }
 
   create(hasReset = true) {
-    super.create()
+    super.create();
     if (hasReset) this.resetDataBoxes();
+  }
+
+  handleBoxSelection(box) {
+    if (box.classList.contains("active") || !box.classList.contains("in-view"))
+      return;
+
+    this.resetActiveBoxes();
+    box.classList.add("active");
+
+    if (
+      this.elements.boxesInView[
+        this.elements.boxesInView.length - 1
+      ].getAttribute("data-box") === box.getAttribute("data-box") &&
+      box.getAttribute("data-box") !==
+        this.elements.boxes[this.elements.boxes.length - 1].getAttribute(
+          "data-box"
+        )
+    ) {
+      this.elements.boxesInView[0].classList.remove("in-view");
+      this.elements.boxes[
+        Number(box.getAttribute("data-box")) + 1
+      ].classList.add("in-view");
+      const firstBox = this.elements.boxes[0];
+      this.elements.boxInnerWrapper.removeChild(
+        this.elements.boxInnerWrapper.firstElementChild
+      );
+      this.elements.boxInnerWrapper.appendChild(firstBox);
+      this.create();
+      return;
+    }
+
+    if (
+      this.elements.boxesInView[0].getAttribute("data-box") ===
+        box.getAttribute("data-box") &&
+      box.getAttribute("data-box") !==
+        this.elements.boxes[0].getAttribute("data-box")
+    ) {
+      this.elements.boxesInView[
+        this.elements.boxesInView.length - 1
+      ].classList.remove("in-view");
+      this.elements.boxes[
+        Number(box.getAttribute("data-box")) - 1
+      ].classList.add("in-view");
+      const lastBox = this.elements.boxes[this.elements.boxes.length - 1];
+      this.elements.boxInnerWrapper.removeChild(
+        this.elements.boxInnerWrapper.lastElementChild
+      );
+      this.elements.boxInnerWrapper.insertBefore(
+        lastBox,
+        this.elements.boxInnerWrapper.firstElementChild
+      );
+      this.create();
+      return;
+    }
+
+    this.create(false);
   }
 }
